a100d175bb62284432ce19a9e9f02ffcb30c6b0e
frank <franklee0512@gmail.com>
2026-03-21 18:30:34 +0800

    Fix Vue config for Vercel deployment

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -2,6 +2,8 @@ const { defineConfig } = require('@vue/cli-service')
 
 module.exports = defineConfig({
   transpileDependencies: true,
+  publicPath: './',
+  outputDir: 'dist',
   devServer: {
     port: 8080,
     host: '0.0.0.0',
